03bf4296458b9d68874a83c95d8c255ed7196d8c
robehind <574544947@qq.com>
2021-09-25 21:05:32 +0800

    ✨ feat(perception): 新增ViT，未测试

--- vnenv/models/perception/__init__.py
+++ vnenv/models/perception/__init__.py
@@ -1,2 +1,8 @@
 from .auxiliary_net import TTRGBpred, SplitRGBPred
 from .simple_cnn import TutorialCNN, SplitNetCNN, House3DCNN
+from .transformer import ViT
+
+__all__ = [
+    'ViT', 'TTRGBpred', 'SplitRGBPred', 'TutorialCNN',
+    'SplitNetCNN', 'House3DCNN'
+]
