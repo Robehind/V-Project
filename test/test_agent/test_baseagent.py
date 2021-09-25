ea8f9f80caf84aab9b3ca84f9a712de886bf5879
robehind <574544947@qq.com>
2021-09-25 21:15:50 +0800

    ✨ feat(agents): 削弱agent功能，扩展动作选择方式
    agent的功能仅用于配合采样，不再完成带有计算图的model forward，不再保存模型。增加对rct的管理，torch.tensor，通过get_rct方法输出numpy格式的rct。增加三种动作选择方式：policy，eps greedy和直接输出。对应单元测试修改。

--- test/test_agent/test_baseagent.py
+++ test/test_agent/test_baseagent.py
@@ -1,10 +1,8 @@
-import glob
-import os
 from vnenv.agents.base_agent import BaseAgent
-from vnenv.utils.init_func import set_seed
 import torch.nn as nn
 import torch
 import numpy as np
+import pytest
 
 
 class TESTmodel(nn.Module):
@@ -13,13 +11,20 @@ class TESTmodel(nn.Module):
         self.f1 = nn.Linear(1, 1)
         self.step = -1
         self.outs = _acts
+        self.rct_shapes = {'lstm': (6, )}
+        self.rct_dtypes = {'lstm': torch.float32}
+        self.rcts = torch.randn(5, 4, 6)
 
-    def forward(self, obs):
+    def forward(self, obs, rct):
         self.step += 1
         out = torch.zeros((4, 5))
         for i, a in enumerate(self.outs[self.step]):
             out[i, a] = 999999
-        return dict(policy=out)
+        return dict(policy=out,
+                    rct={"lstm": self.rcts[self.step].clone()},
+                    q_value=out,
+                    action=torch.tensor(self.outs[self.step])
+                    )
 
 
 class TESTenv:
@@ -27,18 +32,29 @@ class TESTenv:
         self.env_num = 4
 
 
-def test_base_agent():
-    set_seed(1114)
+@pytest.mark.parametrize(["select_func", "select_params"],
+                         [
+                             ['policy_select', []],
+                             ['epsilon_select', [0.00001]],
+                             ['continous', []]
+                         ])
+def test_base_agent(select_func, select_params):
     env = TESTenv()
     model = TESTmodel()
-    agt = BaseAgent(model, env)
+    agt = BaseAgent(model, env, None, select_func, select_params)
+    assert np.allclose(agt.get_rct()['lstm'], 0)
     for i in range(5):
-        idx = agt.action(dict(place=np.array([1, 2, 3])))
+        done = np.array([0, 0, 0, 0])
+        if i == 3:
+            done = np.array([1, 0, 1, 0])
+        idx = agt.action(dict(place=np.array([1, 2, 3])), done=done)
+        if i == 3:
+            assert np.allclose(agt.get_rct()['lstm'][[0, 2]], 0)
+            assert np.allclose(agt.get_rct()['lstm'][[1, 3]],
+                               model.rcts[i][[1, 3]])
+        else:
+            assert np.allclose(agt.get_rct()['lstm'], model.rcts[i])
         assert np.allclose(idx, np.array(_acts[i]))
-    agt.save_model('./', 100)
-    ss = glob.glob('./*.dat')
-    assert len(ss) == 1
-    os.remove(ss[0])
 
 
 _acts = [
@@ -48,5 +64,3 @@ _acts = [
     [1, 3, 1, 0],
     [4, 3, 4, 4]
 ]
-if __name__ == '__main__':
-    test_base_agent()
--- vnenv/agents/base_agent.py
+++ vnenv/agents/base_agent.py
@@ -1,60 +1,75 @@
-from typing import Dict, List, Optional
+from typing import Any, Dict, List, Optional
 import torch
-import torch.nn.functional as F
 import torch.nn as nn
 import numpy as np
 from .abs_agent import AbsAgent
-from vnenv.utils.convert import toTensor
-from vnenv.utils.net_utils import save_model
 from vnenv.environments import VecEnv
+from vnenv.utils.convert import dict2tensor, dict2numpy
+import vnenv.agents.select_funcs as sfcs
 
 
 class BaseAgent(AbsAgent):
-    """单模型智能体。帮助模型完成数据转换并管理将变成输入的模型输出（例如LSTM的隐藏状态）
+    """单模型智能体。管理将变成输入的模型输出（例如LSTM的隐藏状态）\
+       仅用于采样。
     """
     def __init__(
         self,
         model: nn.Module,
         Venv: VecEnv,  # 传环境进来只是为了获取一些参数
-        gpu_ids: Optional[List[int]] = None
+        gpu_ids: Optional[List[int]],
+        select_func: str,
+        select_params: List[Any] = [],  # TODO 暂时只有epsilon需要传,可以是generator
     ):
         self.gpu_ids = gpu_ids
         self.model = model
         self.done = False
         self.proc_num = Venv.env_num
-        # TODO 多gpu训练同一个模型,DataParallel
+        # TODO 多gpu训练同一个模型, DataParallel
         if gpu_ids is not None:
             self.model = self.model.cuda(gpu_ids[0])
-        self.device = next(self.model.parameters()).device
-
-    def model_forward(
-        self,
-        obs: Dict[str, np.ndarray]
-    ) -> Dict[str, torch.Tensor]:
-        """obs is dict. values of obs must in numpy,
-           and first dim is batch dim"""
-        model_input = obs.copy()  # 防止obs被改变，因为obs在外部还被保存了一次
-        for k in model_input:
-            model_input[k] = toTensor(
-                model_input[k], self.device, dtype=torch.float32
-            )
-        return self.model.forward(model_input)
+        self.dev = next(self.model.parameters()).device
+        # if model has recurrent inputs
+        self.rct_shapes = {}
+        self.rct_dtypes = {}
+        self.rct = {}  # values in Tensor
+        # TODO learnable rct
+        if hasattr(model, 'rct_shapes'):
+            self.rct_shapes = model.rct_shapes
+            self.rct_dtypes = model.rct_dtypes
+            self.rct = {
+                k: torch.zeros((self.proc_num, *v),
+                               dtype=self.rct_dtypes[k], device=self.dev)
+                for k, v in self.rct_shapes.items()
+            }
+        self.select = getattr(sfcs, select_func)
+        self.select_params = select_params
 
     def action(
         self,
         obs: Dict[str, np.ndarray],
         done: Optional[np.ndarray] = None
     ) -> List[int]:
-        # 调用action的时候代表是在采样，不计算梯度
+        # no grad when sampling actions
         with torch.no_grad():
-            out = self.model_forward(obs)
-        pi = out['policy']
-        # softmax,形成在动作空间上的分布
-        prob = F.softmax(pi, dim=1).cpu()
-        # 采样
-        action_idx = prob.multinomial(1).numpy().squeeze(1)
-        return action_idx
+            # TODO copy?
+            out = self.model(
+                dict2tensor(obs.copy(), self.dev),
+                self.rct
+            )
+        # if has recurrent states, update and reset
+        if self.rct_shapes != {}:
+            del self.rct
+            self.rct = out['rct']
+            self._reset_rct(done == 1)
+        # action selection
+        return self.select(out, *self.select_params)
 
-    def save_model(self, path2save, steps):
-        title = self.model.__class__.__name__ + '_' + str(steps)
-        save_model(self.model, path2save, title)
+    def _reset_rct(self, idx: np.ndarray):
+        # reset recurrent data specified by idx to 0
+        # TODO learnable init state?
+        assert not isinstance(idx, bool)
+        for v in self.rct.values():
+            v[idx] = 0
+
+    def get_rct(self):
+        return dict2numpy(self.rct)
