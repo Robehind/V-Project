934236fb64837b53aa9185b947658d0c95fab656
robehind <574544947@qq.com>
2021-10-12 17:52:44 +0800

    ✨ feat(q_learner): q_learner 增加新特性
    q learner支持sarsa和double learning，新增稳定demo

--- vnenv/learners/q_learner.py
+++ vnenv/learners/q_learner.py
@@ -2,6 +2,7 @@ from typing import Dict
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from copy import deepcopy
 import numpy as np
 from .rct_learner import RCTLearner
 from .returns_calc import _basic_return
@@ -18,40 +19,64 @@ class QLearner(RCTLearner):
         gamma: float,
         nsteps: int,
         target_model: bool = False,  # TODO double learning
+        sync_freq: int = 100,
         est_type: str = 'q_learning'  # TODO 'sarsa' 'e_sarsa' 'tree_backup'
     ) -> None:
         self.model = model
+        # TODO Safe?
+        self.target_model = deepcopy(model) if target_model else None
+        self.update_cnt = 0
+        self.sync_freq = sync_freq
         self.dev = next(model.parameters()).device
-        self.target_model = target_model
 
         self.optimizer = optimizer
         self.nsteps = nsteps
         self.gamma = np.float32(gamma)
+        self.est_type = est_type
+
+    def sync_model(self):
+        self.target_model.load_state_dict(self.model.state_dict())
+
+    def target_forward(self, obs, rct):
+        # no need for BPTT
+        obs = {k: v.reshape(-1, *v.shape[2:]) for k, v in obs.items()}
+        rct = {k: v.reshape(-1, *v.shape[2:]) for k, v in rct.items()}
+        with torch.no_grad():
+            model_out = self.target_model(
+                dict2tensor(obs, dev=self.dev),
+                dict2tensor(rct, dev=self.dev)
+            )
+        return model_out['q_value']
 
     def learn(
         self,
         batched_exp: Dict[str, np.ndarray]
     ) -> Dict[str, float]:
         obs, rct = batched_exp['obs'], batched_exp['rct']
-        r, a, m = batched_exp['r'], batched_exp['a'], batched_exp['m']
+        r, m = batched_exp['r'][:-1], batched_exp['m'][:-1]
+        a = batched_exp['a']
         exp_num = r.shape[1]
-        # TODO q_a got by target model
         # all data in model_out should in (batch_size, *)
-        if rct == {}:
-            obs = {k: v.reshape(-1, *v.shape[2:]) for k, v in obs.items()}
-            model_out = self.model(dict2tensor(obs, dev=self.dev))
-        else:
-            model_out = self.rct_forward(obs, rct, m)
+        model_out = self.model_forward(obs, rct, m)
         # gather q value
-        q_s = model_out['q_value'][:-exp_num]
-        q_a = q_s.gather(1, toTensor(a.reshape(-1, 1), self.dev))
+        _q_s = model_out['q_value'][:-exp_num]
+        q_a = _q_s.gather(1, toTensor(a[:-1].reshape(-1, 1), self.dev))
+        # q_s got by target model
+        if self.target_model is None:
+            q_s = model_out['q_value']
+        else:
+            q_s = self.target_forward(obs, rct)
         # TODO Q learning时，最后一个要gather最大的；Sarsa时要gather被选择的；
         # Expected sarsa要加权求和,以及树回溯
-        q_last = model_out['q_value'][-exp_num:]
-        last_qa = q_last.max(dim=1)[0]
+        if self.est_type == 'q_learning':
+            q_t = q_s.max(dim=1)[0]
+        elif self.est_type == 'sarsa':
+            q_t = q_s.gather(1, toTensor(a.reshape(-1, 1), self.dev))
+        else:
+            raise NotImplementedError
 
         returns = _basic_return(
-            toNumpy(q_a.reshape(-1, exp_num)), toNumpy(last_qa), r, m,
+            toNumpy(q_t.reshape(-1, exp_num)), r, m,
             self.gamma,
             self.nsteps
         )
@@ -61,6 +86,13 @@ class QLearner(RCTLearner):
         self.optimizer.zero_grad()
         obj_func.backward()
         self.optimizer.step()
+
+        # sync target model
+        if self.target_model is not None:
+            self.update_cnt += 1
+            self.update_cnt %= self.sync_freq
+            if self.update_cnt == 0:
+                self.sync_model()
         return dict(
             obj_func=obj_func.item(),
             q_loss=q_loss.item(),
