f25365a1abee98527bbdbfb570d74a9c2838e61e
robehind <574544947@qq.com>
2021-11-08 19:13:52 +0800

    🦄 refactor(thor_eval): 重构thor_eval

--- vnenv/evalors/thor_eval.py
+++ vnenv/evalors/thor_eval.py
@@ -1,17 +1,21 @@
 from tqdm import tqdm
+from vnenv.environments import VecEnv
 from vnenv.environments.thor_discrete.thordata_utils import get_type
-from vnenv.utils.record_utils import LabelMeanCalcer, thor_data_output
+from vnenv.utils.record_utils import LabelMeanCalcer
 import numpy as np
+from typing import Dict
 
 
 def thor_eval(
-    args,
     agent,
-    env
-):
+    envs: VecEnv,
+    total_epi: int,
+    bar_leave: bool = True,
+    bar_desc: str = ''
+) -> Dict[float, list]:
+
     agent.model.eval()
-    proc_num = args.proc_num
-    total_epi = args.total_eval_epi
+    proc_num = envs.env_num
 
     epis = 0
     env_steps = np.zeros((proc_num))
@@ -19,13 +23,13 @@ def thor_eval(
     false_action_ratio = [[] for _ in range(proc_num)]
     test_scalars = LabelMeanCalcer()
 
-    obs = env.reset()
-    done = np.zeros((env.env_num))
+    obs = envs.reset()
+    done = np.ones((envs.env_num))
 
-    pbar = tqdm(total=total_epi)
+    pbar = tqdm(total=total_epi, desc=bar_desc, leave=bar_leave, unit='epi')
     while epis < total_epi:
         action, _ = agent.action(obs, done)
-        obs_new, r, done, info = env.step(action)
+        obs_new, r, done, info = envs.step(action)
         obs = obs_new
         env_rewards += r
         env_steps += 1
@@ -39,7 +43,14 @@ def thor_eval(
             if done[i] and epis < total_epi:
                 epis += 1
                 pbar.update(1)
-                if args.calc_spl:
+                data = {
+                    'ep_length': env_steps[i],
+                    'SR': t_info['success'],
+                    'total_reward': env_rewards[i],
+                    'epis': 1,
+                    'false_action_ratio': false_action_ratio[i].copy()
+                }
+                if 'min_len' in t_info:
                     if t_info['success']:
                         assert t_info['min_len'] <= env_steps[i],\
                             f"{t_info['min_len']}>{env_steps[i]}"
@@ -47,22 +58,18 @@ def thor_eval(
                         spl = t_info['min_len']/env_steps[i]
                     else:
                         spl = 0
-                data = {
-                    'ep_length:': env_steps[i],
-                    'SR:': t_info['success'],
-                    'SPL:': spl,
-                    'total_reward:': env_rewards[i],
-                    'epis': 1,
-                    'false_action_ratio': false_action_ratio[i]
-                }
-                target_str = \
-                    get_type(t_info['scene_id'])+'/'+t_info['target']
-                for k in [t_info['scene_id'], target_str]:
-                    test_scalars[k].add(data)
+                    data.update(SPL=spl)
+                for k, v in data.items():
+                    for s in [
+                        'Total',
+                        get_type(t_info['scene_id']),
+                        t_info['scene_id'],
+                        t_info['target'],
+                    ]:
+                        test_scalars[k].add({s: v})
                 env_steps[i] = 0
                 env_rewards[i] = 0
                 false_action_ratio[i] = []
 
-    env.close()
     pbar.close()
-    thor_data_output(args, test_scalars)
+    return test_scalars.pop(['epis'])
