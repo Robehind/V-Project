f35479a13adb44a8acfb0d0b0bb42148ed14e8e5
robehind <574544947@qq.com>
2021-12-27 17:34:00 +0800

    ✨ feat(cartpole_exp): 优化示例脚本的速度和代码

--- exp4usage.py
+++ exp4usage.py
@@ -11,49 +11,21 @@ from vnenv.environments.gym_envs import CartPolev1
 from collections import deque
 import gym
 import numpy as np
+import time
 
 
 # Hyperparameters
-n_train_processes = 4
-learning_rate = 0.0001
-update_interval = 5
+proc_num = 8
+exp_length = 4
+learning_rate = 0.001
 gamma = 0.98
-max_train_steps = 300000
+max_train_steps = 100000
 PRINT_INTERVAL = 2000
 reward_scale = 100.0
+speedrun = True
 
-if __name__ == '__main__':
-    env_args = CartPolev1.args_maker(
-        {'event_args': {'r_scale': reward_scale}}, n_train_processes)
-    env_fns = [make_envs(args, CartPolev1) for args in env_args]
-    envs = VecEnv(env_fns)
 
-    model = CartModel(1, 1)
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
-    learner = A2CLearner(model, optimizer, gamma, 1, float("inf"), 1)
-    agt = BaseAgent(model, envs, None, select_func='policy_select')
-    spler = BaseSampler(envs, agt, 20, 5, 4)
-
-    pbar = tqdm(total=max_train_steps)
-    writer = SummaryWriter("../cartpole")
-    tracker = MeanCalcer()
-    step_idx = 0
-
-    rs = deque(maxlen=100)
-    while step_idx < max_train_steps:
-        step_idx += update_interval*n_train_processes
-        exps = spler.sample()
-        loss = learner.learn(exps)
-        tracker.add(loss)
-
-        pbar.update(update_interval*n_train_processes)
-        if step_idx % PRINT_INTERVAL == 0:
-            out = tracker.pop()
-            out.update(spler.pop_records())
-            for k, v in out.items():
-                writer.add_scalar(k, v, step_idx)
-    pbar.close()
+def visualize(agt):
     v_env = gym.make('CartPole-v1')
     for _ in range(5):
         s = v_env.reset()
@@ -65,5 +37,60 @@ if __name__ == '__main__':
                 'obs': np.array(s).reshape(1, -1)}, np.ones((1)))
             s, r, d, info = v_env.step(a[0])
             rr += 1
-        print("reward", rr)
+        print("Returns:", rr)
+    v_env.close()
+
+
+if __name__ == '__main__':
+    env_args = CartPolev1.args_maker(
+        {'event_args': {'r_scale': reward_scale}}, proc_num)
+    env_fns = [make_envs(args, CartPolev1) for args in env_args]
+    envs = VecEnv(env_fns)
+    batch_sz = exp_length*proc_num
+
+    model = CartModel()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    learner = A2CLearner(model, optimizer, gamma, gae_lbd=1,
+                         vf_nsteps=float("inf"), vf_param=1,
+                         vf_loss='smooth_l1_loss',
+                         grad_norm_max=float("inf"),
+                         batch_loss_mean=True)
+    agt = BaseAgent(model, envs, None, select_func='policy_select')
+    spler = BaseSampler(envs, agt, batch_size=exp_length*proc_num,
+                        exp_length=exp_length, buffer_limit=proc_num)
+
+    pbar = tqdm(total=max_train_steps)
+    if not speedrun:
+        writer = SummaryWriter("../cartpole")
+    tracker = MeanCalcer()
+    step_idx = 0
+
+    rs = deque(maxlen=100)
+    if speedrun:
+        st = time.time()
+    while step_idx < max_train_steps:
+        step_idx += batch_sz
+        exps = spler.sample()
+        loss = learner.learn(exps)
+        tracker.add(loss)
+        if speedrun:
+            data = spler.pop_records()
+            for _ in range(int(data['epis'])):
+                rs.append(data['return'])
+            if sum(rs) >= 400:
+                break
+        pbar.update(batch_sz)
+        if not speedrun and step_idx % PRINT_INTERVAL == 0:
+            out = tracker.pop()
+            out.update(spler.pop_records())
+            for k, v in out.items():
+                writer.add_scalar(k, v, step_idx)
+
+    if not speedrun:
+        writer.close()
+    pbar.close()
+    if speedrun:
+        print(f'Finished. Time:{time.time() - st}')
     envs.close()
+    visualize(agt)
