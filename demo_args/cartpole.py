58210cc837e8c5bccdeaf18616aacee422eb6909
robehind <574544947@qq.com>
2021-12-30 17:08:20 +0800

    🎈 perf: cart demo

--- demo_args/cartpole.py
+++ demo_args/cartpole.py
@@ -9,12 +9,12 @@ args.update(
     exps_dir='../demoEXPS',  # 保存所有实验文件夹的路径
     exp_name='cartpole',  # 将用于生成本次实验的实验文件夹的文件名，因此尽量不要包含特殊符号
     exp_dir='',  # 单次实验的完整路径，会根据时间自动生成
-    proc_num=4,  # 进程数
+    proc_num=8,  # 进程数
 
     # train proc params
-    train_steps=300000,  # 指定训练多少frames
+    train_steps=100000,  # 指定训练多少frames
     print_freq=2000,  # 每进行n个frames，就向tensorboardx输出一次训练信息
-    model_save_freq=300000,  # 每进行n个episode，就保存一次模型参数
+    model_save_freq=100000,  # 每进行n个episode，就保存一次模型参数
     train_task={},
 
     # validate params
@@ -41,7 +41,7 @@ args.update(
         gamma=0.98,
         gae_lbd=1,
         vf_nsteps=float("inf"),
-        vf_param=1,
+        vf_param=0.5,
         ent_param=0,
     ),
     model='CartModel',
@@ -51,14 +51,14 @@ args.update(
     ),
     optim='Adam',
     optim_args=dict(
-        lr=0.0001,
+        lr=0.001,
     ),
 
     # exp params
     sampler='BaseSampler',
     sampler_args=dict(
-        batch_size=20,
-        exp_length=5,
-        buffer_limit=4
+        batch_size=32,
+        exp_length=4,
+        buffer_limit=8
     ),
 )
