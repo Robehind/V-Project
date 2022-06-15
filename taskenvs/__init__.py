2ab3e34c40cbcff89b11ec1c3e13d8c9160ea281
robehind <574544947@qq.com>
2022-06-15 20:06:01 +0800

    ✨ feat: env4gcnsavnbase

--- taskenvs/__init__.py
+++ taskenvs/__init__.py
@@ -32,6 +32,17 @@ register(
         info_scene='FloorPlan25')
 )
 register(
+    id='FmScoreTdThor-v0',
+    entry_point='taskenvs.ai2thor_env:ReadFileTDenv',
+    order_enforce=False,
+    kwargs=dict(
+        obs_dict={
+            'res18fm': 'resnet18fm.hdf5',
+            'score': 'resnet50score.hdf5'},
+        target_embedding='glove',
+        info_scene='FloorPlan25')
+)
+register(
     id='FSTdThor-v0',
     entry_point='taskenvs.ai2thor_env:ReadFileTDenv',
     order_enforce=False,
